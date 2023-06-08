00a705fffb66461317de019f1334f796a19934fd
Bùi Văn Thuyên <>
2023-06-08 16:40:24 +0700

    The first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.crypto_portfolio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,11 @@
+import 'package:crypto_portfolio/app/app.dart';
+import 'package:crypto_portfolio/bootstrap.dart';
+import 'package:crypto_portfolio/firebase_options_stg.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await bootstrap(() => const App());
+}
